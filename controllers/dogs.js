bb5357c5bb47e375fce19e3368ab741583c93a85
Tony Ingle <tonyingle@Tonys-MacBook-Air.local>
2021-11-17 12:53:39 -0700

    backend complete

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Dog = require('./../Dog');
 
+// INDEX
 router.get('/', (req, res) => {
 	Dog.find()
 		.then((dogs) => {
@@ -12,10 +13,23 @@ router.get('/', (req, res) => {
 		});
 });
 
+// CREATE
 router.post('/', (req, res) => {
 	Dog.create(req.body).then((dog) => {
 		res.json({ status: 201, dog: dog });
 	});
+});
+
+// DELETE /:id
+router.delete('/:id', (req, res) => {
+	Dog.deleteOne({ _id: req.params.id }).then(() => res.status(204).json());
+});
+
+// PUT /:id
+router.put('/:id', (req, res) => {
+	Dog.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }).then(
+		(dog) => res.status(200).json({ status: 200, dog: dog })
+	);
 });
 
 module.exports = router;
